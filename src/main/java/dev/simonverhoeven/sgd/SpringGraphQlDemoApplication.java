2287cf037f22c1b0a3d100bd81d52384c688df42
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-09-02 14:08:58 +0200

    feat: initial commit

---
+++ src/main/java/dev/simonverhoeven/sgd/SpringGraphQlDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.sgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphQlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphQlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/simonverhoeven/sgd/SpringGraphQlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.sgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
