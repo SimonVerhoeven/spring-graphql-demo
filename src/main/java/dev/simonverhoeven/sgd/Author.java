9185bbb7f5178a3de0da7483eb86bc682b0d5413
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-09-12 19:15:20 +0200

    feat: add subscription for new books

--- src/main/java/dev/simonverhoeven/sgd/Author.java
+++ src/main/java/dev/simonverhoeven/sgd/Author.java
@@ -7,12 +7,13 @@ import java.util.stream.Stream;
 
 public record Author (String id, String firstName, String lastName, String shortBio, String linkedinUrl) {
 
-    private static List<Author> authors = Stream.of(
+    private static final List<Author> authors = Stream.of(
             new Author("a535fe2f-7d06-41bd-bbff-c802e42a8b06", "Robert", "Martin", "Author of 'Clean Code.'", "https://linkedin.com/in/robertmartin"),
             new Author("1e37f5f5-8a2d-439c-9b2a-5ed62d933fad", "Eric", "Freeman", "Co-author of 'Head First Design Patterns.'", "https://linkedin.com/in/ericfreeman"),
             new Author("92672828-f10e-488f-af03-ba3d4137d455", "Douglas", "Crockford", "JavaScript expert and author.", "https://linkedin.com/in/douglascrockford"),
             new Author("19f94cca-fa7a-497d-8b70-76be0ba30f0c", "Herbert", "Schildt", "Experienced Java author.", "https://linkedin.com/in/herbertschildt"),
-            new Author("ea2ed949-b11a-4e4d-9094-dac54b1c4cc9", "Marijn", "Haverbeke", "Author of 'Eloquent JavaScript.'", "https://linkedin.com/in/marijnhaverbeke")
+            new Author("ea2ed949-b11a-4e4d-9094-dac54b1c4cc9", "Marijn", "Haverbeke", "Author of 'Eloquent JavaScript.'", "https://linkedin.com/in/marijnhaverbeke"),
+            new Author("d865fe02-3393-4ebf-a2c4-1ef29f44037f", "Kent", "Beck", "Kent Beck is an American software engineer and the creator of extreme programming.", "https://www.linkedin.com/in/kentbeck/")
     ).collect(Collectors.toCollection(ArrayList::new));
 
     public static Author getById(String id) {
--- src/main/java/dev/simonverhoeven/sgd/BookController.java
+++ src/main/java/dev/simonverhoeven/sgd/BookController.java
@@ -3,7 +3,13 @@ package dev.simonverhoeven.sgd;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Controller
 public class BookController {
@@ -15,5 +21,23 @@ public class BookController {
     @SchemaMapping
     public Author author(Book book) {
         return Author.getById(book.authorId());
+    }
+
+    @SubscriptionMapping("notifyNewBook")
+    public Flux<Book> newBooks() {
+        return Flux.fromIterable(
+                List.of(
+                        new Book("84b5a884-c234-4bec-be9d-951750038276", "978-0321146533", "Extreme Programming Explained", 240, "Introduction to Extreme Programming (XP) methodology.", "October 29, 2004", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("636b1604-21d5-4b6c-97ae-93efa71031b5", "978-0321278654", "Test-Driven Development by Example", 240, "A guide to Test-Driven Development (TDD) practices.", "November 15, 2002", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("1897256c-c523-48a8-be88-141626079c63", "978-0135974445", "Implementation Patterns", 176, "Patterns for writing clean, maintainable code.", "October 22, 2007", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("9277ceb0-ae8f-4c8c-9934-d98e0f8d574e", "978-0201485677", "Smalltalk Best Practice Patterns", 224, "Best practices for programming in Smalltalk.", "November 21, 1996", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("7811392e-acfb-4d2d-9c45-fda0087c778a", "978-0321509369", "Test-Driven Development: By Example (2nd Edition)", 240, "Updated edition of the TDD classic.", "October 17, 2022", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("6c5635e6-7c8a-4eaa-b540-ac7f088402ad", "978-0201558024", "Planning Extreme Programming", 176, "A guide to planning in the XP methodology.", "November 22, 1999", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("4ada5a12-d979-4f96-a090-2cbd6f3933cf", "978-0130676344", "Extreme Programming Explained: Embrace Change (2nd Edition)", 288, "Revised and updated edition of XP principles.", "November 18, 2004", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("c8d7c976-df23-4d7f-bace-ef74dc0af08b", "978-0321336382", "Refactoring: Improving the Design of Existing Code", 464, "A classic on improving code design through refactoring.", "June 25, 1999", "a535fe2f-7d06-41bd-bbff-c802e42a8b06"),
+                        new Book("dc49936f-29e9-494f-a7d3-5591ed99c006", "978-0201710915", "Contributing to Eclipse: Principles, Patterns, and Plug-Ins", 384, "Guidance on contributing to the Eclipse platform.", "April 8, 2003", "a535fe2f-7d06-41bd-bbff-c802e42a8b06")
+                )
+        ).delayElements(Duration.ofSeconds(20));
+
     }
 }
