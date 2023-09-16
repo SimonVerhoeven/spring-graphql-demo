b2359ecd37e1c4666e1c47ae4e4cf0e2a6411952
Simon Verhoeven <verhoeven.simon@gmail.com>
2023-09-16 14:32:30 +0200

    feat: add Introspection report (documentation)

--- src/main/java/dev/simonverhoeven/sgd/GraphQLConfig.java
+++ src/main/java/dev/simonverhoeven/sgd/GraphQLConfig.java
@@ -7,10 +7,17 @@ import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @Configuration
 public class GraphQLConfig {
+//    private static final Logger log = LoggerFactory.getLogger(GraphQLConfig.class);
+
 
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return runtimeWiringConfigurer -> runtimeWiringConfigurer
                 .scalar(ExtendedScalars.Date);
     }
+
+//    @Bean
+//    GraphQlSourceBuilderCustomizer inspectionCustomizer() {
+//        return schemaResourceBuilder -> schemaResourceBuilder.inspectSchemaMappings(reportConsumer -> log.info(reportConsumer.toString()));
+//    }
 }
